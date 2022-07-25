0653ac7e07032aa4322d0ea2af4a8f04606e5bff
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-07-25 14:23:52 +0200

    make it better

--- app/Vue.php
+++ app/Vue.php
@@ -2,10 +2,10 @@
 
 class Vue {
   public static function render(string $page, array $array = []) {
-    extract($array);
     ob_start();
-    require('views/'.$page.'.html.php');
+    extract($array);
+    require APPROOT . "/views/$page.html.php";
     $content = ob_get_clean();
-    require('views/template.html.php');
+    require APPROOT . "/views/template.html.php";
   }
 }
